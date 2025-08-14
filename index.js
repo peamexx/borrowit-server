99ca0375acc26299960ca8af2403aa4b63cddca1
peamexx <peamexx@gmail.com>
2025-08-14 12:28:52 +0900

    feat: 알라딘 api 처리

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+app.use(cors());
+// app.use(express.json());
+
+app.get('/aladin', async (req, res) => {
+  try {
+    const params = new URLSearchParams(req.query);
+
+    const response = await fetch(`${process.env.VITE_ALADIN_URL}${process.env.VITE_ALADIN_BOOKLIST_PATH}?ttbkey=${process.env.VITE_ALADIN_TTBKEY}&QueryType=ItemNewAll&${params.toString()}&output=js&Version=20131101`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch from Aladin API" });
+  }
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
